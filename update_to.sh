71939f836cb87ae78c5e7efb3317f5fc88f6fa54
C.y <yangruihan@vip.qq.com>
2018-11-02 13:59:38 +0800

    update code-snippets and add auto copy script

---
+++ update_to.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DIR_PATH=$HOME/Library/Application\ Support/Code/User/snippets/
+
+if [ ! -d "$DIR_PATH" ]
+then
+    echo "ERROR: Visual Studio Code dose not exist"
+else
+    echo "----- Do Copy -----"
+    echo "$DIR_PATH to $(pwd)"
+
+    cp *.code-snippets "$DIR_PATH"
+
+    echo "--------------------------"
+    echo "----- Update Success -----"
+    echo "--------------------------"
+fi
