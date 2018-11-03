26c60eb72df862c0081a46dbe5248a298add52a5
Rayan <yangruihan@vip.qq.com>
2018-11-03 09:20:28 +0800

    add check system os

--- update_from.sh
+++ update_from.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
-DIR_PATH=$HOME/Library/Application\ Support/Code/User/snippets/
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+echo "Current System is ${machine}"
+
+if [ "${machine}" = "Linux" ]; then
+    DIR_PATH=$HOME/.config/Code/User/snippets/
+else
+    DIR_PATH=$HOME/Library/Application\ Support/Code/User/snippets/
+fi
+
+echo "Visual studio code snippets path: ${DIR_PATH}"
 
 if [ ! -d "$DIR_PATH" ]
 then
